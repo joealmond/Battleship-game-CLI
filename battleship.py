c780ae4f17992c3516785f9ff7e459d1cac6fb91
joealmond <jozsef.mandula@gmail.com>
2020-11-19 13:53:12 +0100

    Refractoring random ships, new user input code, more feature on game ending

--- battleship.py
+++ battleship.py
@@ -28,29 +28,29 @@ import os
 import msvcrt
 
 #Táblázat:
-table = [["   "," A "," B "," C "," D "," E "," F "," G "," H "," I "," J "],
-[" 0 "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "],
-[" 1 "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "],
-[" 2 "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "],
-[" 3 "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "],
-[" 4 "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "],
-[" 5 "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "],
-[" 6 "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "],
-[" 7 "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "],
-[" 8 "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "],
-[" 9 "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "]]
+table = [
+[" _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "],
+[" _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "],
+[" _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "],
+[" _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "],
+[" _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "],
+[" _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "],
+[" _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "],
+[" _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "],
+[" _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "],
+[" _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "]]
 
-tableAirBomb = [["   "," A "," B "," C "," D "," E "," F "," G "," H "," I "," J "],
-[" 0 "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "],
-[" 1 "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "],
-[" 2 "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "],
-[" 3 "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "],
-[" 4 "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "],
-[" 5 "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "],
-[" 6 "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "],
-[" 7 "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "],
-[" 8 "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "],
-[" 9 "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "]]
+tableAirBomb = [
+[" _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "],
+[" _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "],
+[" _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "],
+[" _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "],
+[" _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "],
+[" _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "],
+[" _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "],
+[" _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "],
+[" _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "],
+[" _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "]]
 
 #Hajó típusok, a számértékük a hossukat jelöli:
 CARRIER = 5
@@ -59,11 +59,11 @@ CRUISER = 3
 SUBMARINE = 3
 DESTROYER = 2
 
+a = 0
 SHIPS = [CARRIER, BATTLESHIP, CRUISER, SUBMARINE, DESTROYER]
+SHIPLENGTH = SHIPS[a]
 SHIPFLOTTLENGTH = len(SHIPS)
-ALLRANDOMSHIPSCOORD = []
-SHINKSHIPCOUNT = 0
-AIRBOMBLEFT = 17
+AIRBOMBLEFT = 30
 
 
 #Táblázat rajzoló ciklusfüggvény:
@@ -74,7 +74,11 @@ def table_draw(table):
     print()
     row = int(0)
     col = int(0)
+    print("   ","A ","B ","C ","D ","E ","F ","G ","H ","I ","J ")
+    n = 0
     for i in table[row]:
+        print("", n, "", end="")
+        n += 1
         for i in table:
             print(table[row][col], end="")
             col += 1     
@@ -83,7 +87,7 @@ def table_draw(table):
         print()
     print()
 
-table_draw(table)
+# table_draw(table)
 
 #print("A fenti táblán 5db hadihajót kell elhelyezned. Ad meg a hajók helyzetét egyenként!")
 #print()
@@ -94,154 +98,167 @@ table_draw(table)
 # DESTROYER = input("Add meg az 2 egység hosszú romboló egyik végének kezdő koordinátáját, valamint az irányát meghatározó függőleges vagy vizszintes irányban lévő szomszédos cella koordinátáját: ")
 
 
-# def randDir(shipLength):
+def randomStartCoords():
+    x = random.randint(0, 9)
+    y = random.randint(0, 9)
+    return x,y
 
-direction = []
-dircoord = [0,0]
-x = random.randint(1, 10)
-y = random.randint(1, 10)
-a = 0
-coord = [x,y]
-print("x,y random coordináták:",coord)
-table[coord[0]][coord[1]] = " H "
-shipLength = SHIPS[a]
-while a < SHIPFLOTTLENGTH:
-    a += 1
-print("random coordináták:",x,y)
-if y >= shipLength:
-    dircoord = dircoord[0]+1,dircoord[1]
-    print("jobb",dircoord)
-    direction.append(dircoord)
-    dircoord = dircoord[0]-1,dircoord[1]
-if y <= 11-shipLength:
-    dircoord = dircoord[0]-1,dircoord[1]
-    print("bal",dircoord)
-    direction.append(dircoord)
-    dircoord = dircoord[0]+1,dircoord[1]
-if x >= shipLength:
-    dircoord = dircoord[0],dircoord[1]+1
-    print("fel",dircoord)
-    direction.append(dircoord)
-    dircoord = dircoord[0],dircoord[1]-1
-if x <= 11-shipLength:
-    dircoord = dircoord[0],dircoord[1]-1
-    print("le",dircoord)
-    direction.append(dircoord)
-    dircoord = dircoord[0],dircoord[1]+1
-print("lehetséges irányok:",direction)
-print("random irány:",random.choice(direction))
 
-def ship(shipLength):
-    #table[coord[0]][coord[1]] = " H "
-    dir = randDir(coord,shipLength)
-    print("rdir:",dir)
-    print("Yo")
-    print("hajó hossz:",shipLength)
-    allSHIPSCoord = []
-    shipCoord = [coord[0] + dir[0]][coord[1] + dir[1]]
-    print(shipCoord)
-    allSHIPSCoord.append(shipCoord)
+def randomLegalDirection(x,y,):
+    direction = []
+    print("random coordináták:",x,y)
+    if x >= SHIPLENGTH:
+        directionLeft = [-1,0]
+        direction.append(directionLeft)
+        print("bal", directionLeft)
+    if x <= 10-SHIPLENGTH:
+        directionRight = [1,0]
+        direction.append(directionRight)
+        print("jobb", directionRight)
+    if y >= SHIPLENGTH:
+        directionUp = [0,-1]
+        direction.append(directionUp)
+        print("fel", directionUp)
+    if y <= 10-SHIPLENGTH:
+        directionDown = [0,1]
+        direction.append(directionDown)
+        print("le", directionDown)
+    print("lehetséges irányok:",direction)
+    randomDirection = random.choice(direction)
+    print("random irány:",randomDirection)
+    return randomDirection
+
+
+def checkShipPlace(x,y,randomDirection,SHIPLENGTH):
+    randomDirection = randomDirection
     i = 0
-    while i < shipLength:
-        if table[shipCoord[0]][shipCoord[1]] != " H ":
+    while i < SHIPLENGTH:
+        if table[y + randomDirection[1]*i][x + randomDirection[0]*i] == " _ ":
             i += 1
-            print("Van hely?:",i)
-        else:
-            i = 0
-            coord = randCoord()
-            shipCoord = dir[0],dir[1]
-            dir = randDir(coord,shipLength)
-            print("Újragenerálás!")
-        i = 0
-        while i <= shipLength:
-            print("Yoo")
-            table[dir[0]][dir[1]] = " H "
-            shipCoord = dir[0],dir[1]
-            allSHIPSCoord.append(shipCoord)
-            i += 1 
-    return allSHIPSCoord   
+        elif table[y + randomDirection[1]*i][x + randomDirection[0]*i] != " _ ":
+            break
+    return i
 
-def randSHIPS():
-    global ALLRANDOMSHIPSCOORD
+
+def placeShip(x,y,randomDirection,SHIPLENGTH):
+    randomShipCoords = []
+    i = 0
+    while i < SHIPLENGTH:
+        table[y + randomDirection[1]*i][x + randomDirection[0]*i] = " H "
+        shipCoords = (y + randomDirection[1]*i, x + randomDirection[0]*i)
+        randomShipCoords.append(shipCoords)
+        print("rand ship coords:", randomShipCoords)
+        i += 1
+    return randomShipCoords
+
+
+def placeRandomShips():
+    allRandomShipsCoords = []
     a = 0
     while a < SHIPFLOTTLENGTH:
-        allSHIPSCoord = []
-        allSHIPSCoord = ship(SHIPS[a])
-        ALLRANDOMSHIPSCOORD.append(allSHIPSCoord)
-        print("Leraktam a hajót!")
-        print("a értéke(max. a hajók száma.):",a)
-        a += 1
-    return ALLRANDOMSHIPSCOORD
+        SHIPLENGTH = SHIPS[a]
+        x,y = randomStartCoords()
+        randomDirection = randomLegalDirection(x,y)
+        checkShipPlaceValue = checkShipPlace(x,y,randomDirection,SHIPLENGTH)
+        print("check ship place value:",checkShipPlaceValue == SHIPLENGTH)
+        if checkShipPlaceValue == SHIPLENGTH:
+            placeShipCoords = placeShip(x,y,randomDirection,SHIPLENGTH)
+            print("place ship Coords:",placeShipCoords == SHIPLENGTH)
+            if len(placeShipCoords) == SHIPLENGTH:
+                allRandomShipsCoords.append(placeShipCoords)
+                a += 1
+                print("a értéke:", a)
+        print("köv hajótípus:", SHIPLENGTH)
+    return allRandomShipsCoords
+
 
 def inputCoord():
-    char = " abcdefghijq"
-    char1 = " 0123456789q"
+    char = "abcdefghijq"
+    char1 = "0123456789q"
     coordStr1 = "_"
     coordStr2 = "_"
     coord = [int(-1),int(-1)]
-    while coord[0] == -1 and coord[0] != 11:
+    while coord[0] == -1 and coord[0] != 10:
         print("Adj meg egy értéket a vízszintes tengelyen a-j-ig:")
         coordStr1 = msvcrt.getwch()
         print("A koordináta:",coordStr1,",",coordStr2)
         coordStr1.casefold()
-        coord[0] = char.find(coordStr1[0],1,12)
-    while coord[1] == -1 and coord[0] != 11:
+        coord[0] = char.find(coordStr1[0],0,11)
+    while coord[1] == -1 and coord[1] != 10 and coord[0] != 10:
         print("Adj meg egy értéket a függőleges tengelyen 0-9-ig:")
         coordStr2 = msvcrt.getwch()
         print("A koordináta:",coordStr1,",",coordStr2)
         coordStr2.casefold()
-        coord[1] = char1.find(coordStr2[0],1,12)
+        coord[1] = char1.find(coordStr2[0],0,11)
+    print("A koordináta értéke:",coord)
     return coord
 
-def sink(Air_y,Air_x):
-    AirY = int(0)
-    AirX = int(0)
+
+def sink(Air_y,Air_x,allRandomShipsCoords,shinkShipCount):
+    shinkShipCount = shinkShipCount
+    AirY = 0
+    AirX = 0
     s = []
     shipPart = []
-    global SHINKSHIPCOUNT
-    for i in range(0,11):
-        for k in range(0,11):
-            if tableAirBomb[AirX][AirY] == " T ":
+    for i in range(0,10):
+        for k in range(0,10):
+            if table[AirX][AirY] == " T ":
                 s = (AirX,AirY)
                 shipPart.append(s)
             AirX += 1     
         AirX = 0
         AirY += 1
-    for i in ALLRANDOMSHIPSCOORD:
+    print(shipPart)
+    for i in allRandomShipsCoords:
         if sorted(tuple(i)) == sorted(tuple(shipPart)):
-            SHINKSHIPCOUNT += 1
+            shinkShipCount += 1
             for i in shipPart:
                 tableAirBomb[i[0]][i[1]] = " S "
                 table[i[0]][i[1]] = " S "
+    return shinkShipCount
+
 
 def AirBombGame():
-    global airBomb
+    shinkShipCount = 0
+    allRandomShipsCoords = placeRandomShips()
+    global AIRBOMBLEFT
     print("A számítógép elhelyezte az öt hajóból álló flottát, kezdődhet a játék!" )
     print()
     print("q = Kilépés")
     print()
-    print("A légitámadásból", airBomb-1, "bombád maradt.")
+    print("A légitámadásból", AIRBOMBLEFT-1, "bombád maradt.")
+    table_draw(table)
     coord = [0,0]
     AirY = 0
     AirX = 0
     i = 0
-    while i <= airBomb-1:
+    while i <= AIRBOMBLEFT-1:
+        coord = inputCoord()
+        AirY = coord[0]
+        AirX = coord[1]
+        table_draw(tableAirBomb)
+        if AirY == 10 or AirX == 10:
+            print("Kilépés..")
+            break
         if table[AirX][AirY] == " H ":
             table[AirX][AirY] = " T "
             tableAirBomb[AirX][AirY] = " T "
-            sink(AirY,AirX)
+            shinkShipCount = sink(AirY,AirX,allRandomShipsCoords,shinkShipCount)
+            if shinkShipCount == SHIPFLOTTLENGTH:
+                break
             table_draw(tableAirBomb)
+            print("elsűlyeszett hajók száma:", shinkShipCount)
             print("Talált!")
             print()
             print("q = Kilépés")
             print()
-            print("A légitámadásból", airBomb-i, "bombád maradt.")
+            print("A légitámadásból", AIRBOMBLEFT-i, "bombád maradt.")
         elif tableAirBomb[AirX][AirY] == " O " or tableAirBomb[AirX][AirY] == " T ":
             print("Ide már lőttél!")
             print()
             print("q = Kilépés")
             print()
-            print("A légitámadásból", airBomb-i, "bombád maradt. Ne pazarolj!")
+            print("A légitámadásból", AIRBOMBLEFT-i, "bombád maradt. Ne pazarolj!")
         elif table[AirX][AirY] == " _ ": 
             table[AirX][AirY] = " O "
             tableAirBomb[AirX][AirY] = " O "
@@ -250,25 +267,19 @@ def AirBombGame():
             print()
             print("q = Kilépés")
             print()
-            print("A légitámadásból", airBomb-i, "bombád maradt.")
-        coord = inputCoord()
-        AirY = coord[0]
-        AirX = coord[1]
-        table_draw(tableAirBomb)
-        if AirY == 11 or AirX == 11:
-            print("Kilépés..")
-            break
+            print("A légitámadásból", AIRBOMBLEFT-i, "bombád maradt.")
         i += 1
+    return shinkShipCount
         
 
-# randSHIPS()
-
-# AirBombGame()
+shinkShipCount = AirBombGame()
 
 table_draw(table)
 
-if SHINKSHIPCOUNT > 0:
-    print("Gratulálunk!", SHINKSHIPCOUNT, "hajót sűlyeszetél el a flottából!")
+if shinkShipCount > 0 and shinkShipCount < SHIPFLOTTLENGTH:
+    print("Gratulálunk!", shinkShipCount, "hajót sűlyeszetél el a flottából!")
+elif shinkShipCount == SHIPFLOTTLENGTH:
+    print("Gratulálunk! A teljes flottát elsűlyesztetted!")
 else:
     print("Legközelebbre több szerencsét kívánunk...")
 print()
@@ -279,10 +290,11 @@ print("Viszlát!")
 # webes verzió elkészítése
 
 # Tervezett funkciók:
+# hajók betűjele különböző legyen a táblán
 # Játktípus választás, légitámadás, vagy hajócsata! Játék ismétlése, menü...
 
 # Hibajavítás:
-# nem tökéletes az elkerülő algoritmus? Lehetne plusz egy karakter távolság a hajók között. (ritkán egyes hajók 1 egységgel rövidebbek!, talána legkisebbel van a baj...) Ilyenkor a sink sem működik..
+# Bombák száma esetlen
 
 # Dokumentáció:
 # kommentelni a részeket, áttekinthetővé tenni a kódot.
